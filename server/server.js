00175c27c17add19f902eb6bb5feedb33982e3a1
JeetuPalhub <jeetupal.pal31@gmail.com>
2026-01-16 19:11:56 +0530

    feat: add MongoDB connection setup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./db");
+
 const app = express();
+
+connectDB();
 
 app.use(express.json());
 
@@ -10,6 +15,8 @@ app.get("/health", (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
