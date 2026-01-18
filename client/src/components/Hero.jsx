4a17f1b7fe77c36649f341ad303bb794802312ad
Syed Afrid <83272741+s-afrid@users.noreply.github.com>
2026-01-18 16:59:43 +0530

    fix: update hero banner styles and reorder elements for better layout

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,18 +7,19 @@ const Hero = () => {
     <div className='flex flex-col hero-banner h-[calc(100vh-110px)] w-screen'>
         <video className='absolute top-0 -z-11 h-screen w-screen object-cover' src={heroUrl} muted autoPlay />
         <div className='absolute top-0 -z-10 h-screen w-screen bg-black opacity-40'></div>
-        <div className="w-[96%] h-px bg-black border-0 self-center"></div>
-        <ul className='flex gap-7 p-7 items-center justify-end montserrat'>
-          <li>Shop All</li>
-          <li>Rent</li>
-          <li>Community</li>
-          <li>Sell With Us</li>
-          <li>Plan & Price</li>
-        </ul>
-        <div className='absolute top-[70%] left-[5%] flex flex-col gap-4'>
-        <h1 className='text-white text-5xl playfair-display'>Discovery Luxury at Otulia</h1>
-        <p className='text-white montserrat text-lg'>Find Your Dream Car, Bike, Yacht, House and More.</p>
+        <div className="w-[96%] h-px bg-white border-0 self-center"></div>
+        <div className='absolute top-[9%] left-[5%] flex flex-col gap-4'>
+          <h1 className='text-white text-5xl playfair-display'>Discovery Luxury at Otulia</h1>
+          <p className='text-white montserrat text-lg'>Find Your Dream Car, Bike, Yacht, House and More.</p>
         </div>
+        <ul className='flex gap-7 p-7 items-center justify-end montserrat text-white'>
+          <li className='cursor-pointer'>Shop All</li>
+          <li className='cursor-pointer'>Rent</li>
+          <li className='cursor-pointer'>Community</li>
+          <li className='cursor-pointer'>Sell With Us</li>
+          <li className='cursor-pointer'>Plan & Price</li>
+        </ul>
+        
     </div>
 </>
   )
