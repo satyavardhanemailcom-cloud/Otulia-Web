48e6500d9575a55a69dff9ef2f334b33da401f2d
Syed Afrid <83272741+s-afrid@users.noreply.github.com>
2026-01-18 21:29:29 +0530

    added reponsiveness to hero banner

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Cart from './navbar/Cart'
 import Search from './navbar/Search'
 import LoginButton from './navbar/LoginButton'
+import HamburgerMenu from '../assets/icons/hamburger_menu.svg'
 
 const Navbar = () => {
   return (
     <nav className='flex items-center justify-between p-6'>
-      <img className='w-[200px] h-[60px]' alt="logo" src="logos/logo.png" title='Otulia' />
-      <ul className='flex items-center justify-center gap-8'>
+      <img className='w-[140px] md:w-[200px] h-[40px] md:h-[60px]' alt="logo" src="logos/logo.png" title='Otulia' />
+      <img src={HamburgerMenu} alt="hamburgermenu" />
+
+
+      <ul className='hidden md:flex items-center justify-center gap-8'>
         <li>
             <Search />
           </li>
