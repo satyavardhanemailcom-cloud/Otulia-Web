338362080e8b6cef28aa914ac09b8fe53c55a0f3
s-afrid <afrid0052@gmail.com>
2026-01-21 12:54:18 +0530

    feat(server): Add CORS middleware and configure for localhost:5173

--- server/server.js
+++ server/server.js
@@ -1,24 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./db");
+const cors = require("cors");
 
 // route import
 const homeRoutes = require("./routes/home.routes");
 const listingRoutes = require("./routes/listing.routes");
 const assetsRoutes = require("./routes/assets.routes");
 const authRoutes = require("./routes/auth.routes");
+const trendingRoutes = require("./routes/trending.routes");
 
 const app = express();
 
 connectDB();
 
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 // routes register
 app.use("/api/auth", authRoutes);
 app.use("/api/home", homeRoutes);
 app.use("/api/listings", listingRoutes);
 app.use("/api/assets", assetsRoutes);
+app.use("/api/trending", trendingRoutes);
 
 
 app.get("/health", (req, res) => {
